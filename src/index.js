7d1469e94f1e6a8d68fa654c538fcd2766ff0973
palak varshney <varshneypalak58@gmail.com>
2024-08-25 12:41:03 +0530

    update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { store } from "./redux/Store";
-import { ToastContainer } from "react-toastify";
+
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "../src/redux/AuthContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
